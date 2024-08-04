85aaf93baf4419ff19c2f5ba8a0dfbd02f759110
raudsteinns <raudsteinn@gmail.com>
2024-08-04 21:54:56 +0900

    ♻️ childrenからpropsに変更

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Guide>about</Guide>
+        <Guide title="about" />
         <NextLogo />
         <Links />
       </main>
--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 interface Props {
-  children: React.ReactNode;
+  title: string;
 }
 
 const Guide: React.FC<Props> = (props) => {
-  const { children } = props;
+  const { title } = props;
   return (
     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Get started by editing&nbsp;
-        <code className="font-mono font-bold">{`src/app/${children}.tsx`}</code>
+        <code className="font-mono font-bold">{`src/app/${title}.tsx`}</code>
       </p>
       <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
         <a
