2036ebb40b43ae22c8bc433ba63a3c63a667b040
raudsteinns <raudsteinn@gmail.com>
2024-08-07 19:34:19 +0900

    🐛 子ページレイアウトの記述ミス修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
   );
