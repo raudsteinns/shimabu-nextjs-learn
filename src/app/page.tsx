6fc011ca7a54b472465f2fcf0863905ea7612877
raudsteinns <raudsteinn@gmail.com>
2024-08-07 18:05:28 +0900

    ⚗️ クリックイベントの確認（あとで削除予定）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,25 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
+import { useCallback } from "react";
 
 const Index: React.FC = () => {
+  const foo = 1;
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      console.log((e.target as HTMLAnchorElement).href);
+      e.preventDefault();
+      alert(foo);
+    },
+    []
+  );
+
   return (
     <>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </>
