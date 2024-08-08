c36a0833c044e6a0f76a52b502a30d30b871bf2f
raudsteinns <raudsteinn@gmail.com>
2024-08-08 18:07:39 +0900

    ✨ useStateで文字列と真偽地について演習

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useRef, useState } from "react";
 
 const Index: React.FC = () => {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const bodyRef = useRef<HTMLElement | null>(null);
   useEffect(() => {
@@ -27,20 +29,31 @@ const Index: React.FC = () => {
     };
   }, [count]);
 
-  const handleClick = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      if (count < 10) {
-        setCount((prevCount) => prevCount + 1);
-      }
-    },
-    [count]
-  );
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, [setIsShow]);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    if (newValue.length > 5) {
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
   return (
     <>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </>
